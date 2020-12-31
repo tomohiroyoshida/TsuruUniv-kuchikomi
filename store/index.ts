89a51149553937c5ad9753ac01e14e4cd4a2b54a
tomo <tomohiroyoshida0511@gmail.com>
2020-12-31 18:25:09 +0900

    ADD: store追加

---
+++ store/index.ts
@@ -0,0 +1,14 @@
+export const state = () => ({
+  // ユーザーの情報
+  user: {
+    userId: 'user0001',
+    username: 'サンプル太郎',
+    loggedIn: false
+  },
+  //  講義のリスト
+  classes: []
+})
+
+export const mutations = {}
+
+export const actions = {}
