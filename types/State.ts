1b48eb5066fe3041ad1c5d43c88cdbd847fe12da
tomo <tomohiroyoshida0511@gmail.com>
2021-01-08 03:47:42 +0900

    CHANGE: 型定義ファイルをexport inport して使う形に変更

---
+++ types/State.ts
@@ -0,0 +1,26 @@
+// ユーザーの情報
+export interface User {
+  userId: string
+  username: string
+  loggedIn: boolean
+}
+
+// 講義の情報
+export interface Class {
+  id: string
+  title: string
+  teacher: string
+  dayOfWeek: string
+  period: string
+  createdAt: Date
+}
+
+// クチコミ
+export interface Kuchikomi {
+  title: String
+  classYear: string
+  rating: number
+  content: string
+  user: string
+  createdAt: Date
+}
