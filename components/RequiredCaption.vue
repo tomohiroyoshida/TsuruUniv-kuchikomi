a0654e07d676d57ff6912fa031a63e1e7942ce08
tomo <tomohiroyoshida0511@gmail.com>
2021-01-11 20:01:38 +0900

    ADD: 必須項目を表すテキストのコンポーネント RequiredCaption 作成

---
+++ components/RequiredCaption.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="required-caption text-caption my-1 ml-3">
+    <span class="red--text">* </span>
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'RequiredCaption',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {}
+})
+</script>
+
+<style scoped></style>
