4ac05d05f4a2cb5ca01bc81dbb63c296d96625ea
tomo <tomohiroyoshida0511@gmail.com>
2021-01-15 05:07:18 +0900

    ADD: SnackBar コンポーネント作成

---
+++ components/SnackBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-snackbar
+    :value="value"
+    :color="color"
+    text
+    bottom
+    rounded
+    timeout="4000"
+    class="snack-bar"
+  >
+    {{ text }}
+    <template v-slot:action="{ attrs }">
+      <v-btn text fab small v-bind="attrs" @click="$emit('input', false)">
+        <v-icon> mdi-close </v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'SnackBar',
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {}
+})
+</script>
+
+<style scoped></style>
