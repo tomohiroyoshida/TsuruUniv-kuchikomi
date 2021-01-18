9f50926b91d9b1c64da932304006251525b7a9f1
tomo <tomohiroyoshida0511@gmail.com>
2021-01-19 03:00:21 +0900

    ADD: 404mページ追加

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,28 +1,43 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404" class="mt-15">
-      {{ pageNotFound }}
+  <v-app>
+    <h1 v-if="error.statusCode === 404" class="mt-14">
+      <v-card
+        outlined
+        color="#F5F5F5"
+        height="200"
+        rounded="30"
+        class="px-4 py-8 text-center"
+      >
+        <div class="mb-3">お探しのページが見つかりません。</div>
+        <div class="text-subtitle-1 mb-3">URLが正しいか確認してください。</div>
+        <NuxtLink to="/" class="text-h6"> ホーム画面へ戻る</NuxtLink>
+      </v-card>
+      <v-img contain max-height="500" src="/cat_PC.jpeg" />
+      <div class="text-caption text-center">
+        illustrated by
+        <a href="https://twitter.com/sgrn4902" target="_blank" class="ml-1"
+          >shigureni</a
+        >
+      </div>
     </h1>
+
     <h1 v-else>
-      {{ otherError }}
+      <v-img contain max-height="400" src="/cat_PC.jpeg" />
+      <div class="text-center">
+        エラーが起きました。
+        <NuxtLink to="/" class="text-subtitle-1"> ホーム画面へ戻る</NuxtLink>
+      </div>
     </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
   </v-app>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: 'error',
   props: {
     error: {
       type: Object,
       default: null
-    }
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
     }
   },
   head() {
@@ -38,5 +53,17 @@ export default {
 <style scoped>
 h1 {
   font-size: 20px;
+}
+.card {
+  margin: auto 10px;
+}
+
+.img {
+  position: relative;
+}
+.name {
+  position: absolute;
+  right: 30%;
+  bottom: 20px;
 }
 </style>
