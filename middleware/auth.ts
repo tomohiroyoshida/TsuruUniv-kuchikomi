2f5b89f8c6e20ed1bac83d9df8fe894517bb42fd
tomo <tomohiroyoshida0511@gmail.com>
2021-02-02 09:34:03 +0900

    ADD: 認証状態を常にチェック

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,8 @@
 import firebase from 'firebase'
 import { defineNuxtMiddleware } from '@nuxtjs/composition-api'
 
-export default defineNuxtMiddleware(({ store }) => {
+export default defineNuxtMiddleware(({ store, route, redirect }) => {
+  const excludedPaths = ['/create', '/create/new-class']
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       const userInfo = {
@@ -14,6 +15,11 @@ export default defineNuxtMiddleware(({ store }) => {
       store.dispatch('setUser', userInfo)
       console.debug('login user: ', userInfo)
     } else {
+      const currentPath = route.path
+      console.debug('path', currentPath)
+      if (excludedPaths.includes(currentPath)) {
+        redirect('/login')
+      }
       console.debug('no signing user')
     }
   })
