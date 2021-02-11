241e0d0034b8fddcab98edda756bc455ecb32784
tomo <tomohiroyoshida0511@gmail.com>
2021-02-11 09:25:50 +0900

    dont fetch classes data from firstore if store is not []

--- plugins/classes.ts
+++ plugins/classes.ts
@@ -4,13 +4,14 @@ import { Class } from '@/types/State'
 
 export default defineNuxtPlugin(async ({ store }) => {
   const fetchedClasses: Class[] = []
-  await db
-    .collection('classes')
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        fetchedClasses.push(doc.data() as Class)
+  if (store.getters.classes !== [])
+    await db
+      .collection('classes')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          fetchedClasses.push(doc.data() as Class)
+        })
       })
-    })
   store.dispatch('setClasses', fetchedClasses)
 })
