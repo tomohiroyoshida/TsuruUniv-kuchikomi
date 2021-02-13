1924792ecc44385762275f0915e8cb89317bc01a
tomo <tomohiroyoshida0511@gmail.com>
2021-02-13 16:17:08 +0900

    FAILED: add profile

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -6,23 +6,31 @@ export default defineNuxtMiddleware(({ store, route, redirect }) => {
   // ログインしていないとアクセスできないパス
   const loggedInPaths = [
     '/create',
-    '/create/',
+    '/create/*',
     '/create/new-class',
     '/create/new-class/',
     '/search',
     '/search/'
   ]
+
+  // const db = firebase.firestore()
+  // const setUserInfo = async (userInfo: User): Promise<void> => {
+  //   await db.collection('users').doc(userInfo.uid).set(userInfo)
+  // }
   // 認証状態の監視
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // TODO: ユーザ名は自由に変えられるようにする
       const userInfo: User = {
         uid: user.uid,
         username: user.displayName || '匿名ユーザー',
-        photoURL: user.photoURL,
-        loggedIn: true
+        photoURL: store.getters.user.photoURL
+          ? store.getters.user.photoURL
+          : user.photoURL,
+        department: store.getters.user.department,
+        faculty: store.getters.user.faculty
       }
       store.dispatch('setUser', userInfo)
+      // setUserInfo(userInfo)
     } else {
       const currentPath = route.path
       if (loggedInPaths.includes(currentPath)) {
