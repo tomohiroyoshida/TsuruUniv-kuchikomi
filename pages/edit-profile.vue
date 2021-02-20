2107c0a9cd9662d43eb226c671404685ffaf0ac5
tomo <tomohiroyoshida0511@gmail.com>
2021-02-20 23:25:13 +0900

    fix design (padding)

--- pages/edit-profile.vue
+++ pages/edit-profile.vue
@@ -4,7 +4,7 @@
       <v-row no-gutters justify="center">
         <v-col cols="11" class="mt-12">
           <div class="text-h6 d-flex justify-center my-3 font-weight-bold">
-            プロフィール編集
+            プロフィール設定
           </div>
           <!-- ユーザーネーム -->
           <section>
@@ -15,31 +15,9 @@
               color="primary"
             />
           </section>
-          <v-row no-gutters justify="center">
-            <v-col cols="6">
-              <section>
-                <TextCaption title="学部" class="mb-1" />
-                <SelectInput
-                  v-model="user.faculty"
-                  :items="FACULTIES"
-                  color="primary"
-                  class="pr-1"
-                />
-              </section>
-            </v-col>
-            <v-col cols="6">
-              <section>
-                <TextCaption title="学科" class="mb-1" />
-                <SelectInput
-                  v-model="user.department"
-                  :items="DEPARTMENTS"
-                  color="primary"
-                />
-              </section>
-            </v-col>
-          </v-row>
-          <!-- TODO: 画像 -->
-          <!-- <TextCaption title="プロフィール画像" class="mb-1" />
+
+          <!-- 画像 -->
+          <TextCaption title="プロフィール画像" class="mb-1" />
           <v-file-input
             v-model="imageFile"
             outlined
@@ -48,17 +26,17 @@
             label="画像を選択"
             prepend-icon=""
             prepend-inner-icon="mdi-image"
-            @change="makephotoURL"
+            @change="makePhotoURL"
           />
-
           <div class="d-flex justify-center">
             <v-img
-              :src="photoURL ? photoURL : originalPhotoURL"
+              :src="photoURL || originalPhotoURL"
               max-height="100"
               max-width="100"
             />
           </div>
-          <div class="text-center text-caption">プレビュー</div> -->
+          <div class="text-center text-caption">プレビュー</div>
+
           <!-- 送信・キャンセルボタン -->
           <div class="d-flex justify-center py-3 mt-5">
             <AppBtn
@@ -71,6 +49,8 @@
             </AppBtn>
           </div>
         </v-col>
+
+        <!-- ダイアログ・スナックバー -->
         <ConfirmDialog
           v-model="isOpenUpdateConfirm"
           text="編集"
@@ -96,22 +76,9 @@ import { defineComponent, ref } from '@nuxtjs/composition-api'
 import db from '@/plugins/firebase'
 import { User } from '@/types/State'
 
-const FACULTIES = [
-  { text: '文学部', value: 'letters' },
-  { text: '教養学部 ', value: 'liberalArts' },
-  { text: '大学院', value: 'graduate' }
-]
-const DEPARTMENTS = [
-  { text: '国文学科', value: 'japanese' },
-  { text: '英文学科', value: 'english' },
-  { text: '比較文化学科', value: 'comparativeCulture' },
-  { text: '国際教育学科', value: 'globalEducation' },
-  { text: '学校教育学科', value: 'teacherEducation' },
-  { text: '地域社会学科', value: 'communitySociety' }
-]
 const RULES = {
   required: [(v: string) => !!v || 'この欄の入力は必須です']
-}
+} as const
 
 export default defineComponent({
   name: 'profile',
@@ -124,30 +91,30 @@ export default defineComponent({
     const isOpenErrorSnackbar = ref(false)
 
     const imageFile = ref<File>()
-    const originalPhotoURL = ref()
-    originalPhotoURL.value = root.$store.getters.user.photoURL
     const photoURL = ref<ArrayBuffer | string>()
-    const makephotoURL = (file: File) => {
+    const originalPhotoURL = Object.assign({}, user.photoURL)
+
+    // 変更したプロフィールを全ての口コミに反映
+
+    const makePhotoURL = (file: File): void => {
       if (file) {
         const fr = new FileReader()
         fr.readAsDataURL(file)
         fr.addEventListener('load', () => {
           if (fr.result) photoURL.value = fr.result
-          console.debug('url', photoURL.value)
         })
       }
     }
-    const updateProfile = async () => {
+    const updateProfile = async (): Promise<void> => {
       isOpenErrorSnackbar.value = false
       isOpenSuccessSnackbar.value = false
       //  更新処理
-      // if (photoURL.value) {
       const updatedUser: User = {
         uid: user.uid,
         username: user.username,
-        department: user.department,
-        faculty: user.faculty,
-        photoURL: root.$store.getters.user.photoURL
+        photoURL:
+          photoURL.value ||
+          'https://storage.googleapis.com/studio-cms-assets/projects/RQqJDxPBWg/s-1000x1000_v-fs_webp_eb270a46-5d4c-484e-ada2-a42a7f45f182.webp'
       }
       try {
         const docRef = db.collection('users').doc(user.uid)
@@ -160,14 +127,9 @@ export default defineComponent({
         isOpenUpdateConfirm.value = false
         isOpenErrorSnackbar.value = true
       }
-      // } else {
-      //   isOpenUpdateConfirm.value = false
-      //   isOpenErrorSnackbar.value = true
-      // }
     }
+
     return {
-      FACULTIES,
-      DEPARTMENTS,
       RULES,
       user,
       imageFile,
@@ -178,7 +140,7 @@ export default defineComponent({
       isOpenUpdateConfirm,
       isOpenSuccessSnackbar,
       isOpenErrorSnackbar,
-      makephotoURL
+      makePhotoURL
     }
   }
 })
