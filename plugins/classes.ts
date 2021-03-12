7223ec68e885fccd62ccb86873c0c1f43dc6372c
tomo <tomohiroyoshida0511@gmail.com>
2021-03-12 14:33:00 +0900

    fix: remove meaningless code

--- plugins/classes.ts
+++ plugins/classes.ts
@@ -1,15 +1,9 @@
 import { defineNuxtPlugin } from '@nuxtjs/composition-api'
-import firebase from 'firebase'
 import db from '@/plugins/firebase'
 import { Class } from '@/types/State'
 
 export default defineNuxtPlugin(async ({ store }) => {
   const fetchedClasses: Class[] = []
-  const ref = db.collection('classes').doc('21gpXsJ5bXL7NR1p9z5p')
-  ref.update({
-    dayOfWeek: firebase.firestore.FieldValue.delete(),
-    period: firebase.firestore.FieldValue.delete()
-  })
   if (store.getters.classes !== []) {
     await db
       .collection('classes')
