2e883713a503247d53c4bb5e1064b41e5a42ad54
tomo <tomohiroyoshida0511@gmail.com>
2021-03-22 05:47:21 +0900

    fix: fix order of TAGS

--- data/TAGS.ts
+++ data/TAGS.ts
@@ -5,11 +5,11 @@ interface Tag {
   color: string // チップの色
 }
 export const TAGS: Tag[] = [
-  { text: 'ラクタン', value: 'easy', color: 'yellow darken-3' },
-  { text: 'オニタン', value: 'difficult', color: 'pink darken-4' },
   { text: '般教', value: 'general', color: 'brown darken-1' },
   { text: '体育', value: 'PE', color: 'blue-grey darken-2' },
   { text: 'ゼミ', value: 'zemi', color: 'cyan darken-1' },
+  { text: 'ラクタン', value: 'easy', color: 'yellow darken-3' },
+  { text: 'オニタン', value: 'difficult', color: 'pink darken-4' },
   { text: '国文', value: 'japanese', color: 'red darken-1' },
   { text: '英文', value: 'english', color: 'orange darken-3' },
   { text: '比文', value: 'comparativeCulture', color: 'teal darken-2' },
