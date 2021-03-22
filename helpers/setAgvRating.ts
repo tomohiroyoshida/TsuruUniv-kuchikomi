a4ad058b711b318709311928ca004cccbe9af65a
tomo <tomohiroyoshida0511@gmail.com>
2021-03-23 01:53:20 +0900

    remove: remove console.debug

--- helpers/setAgvRating.ts
+++ helpers/setAgvRating.ts
@@ -4,8 +4,6 @@
 import db from '@/plugins/firebase'
 
 export async function setAvgRating(classId: string): Promise<void> {
-  console.debug('setAgvRating in the classId: ', classId)
-
   const avgRatingsArr: number[] = [] // おすすめ度を格納する配列
   // 授業の全部クチコミのおすすめ度を取得
   await db
@@ -16,7 +14,6 @@ export async function setAvgRating(classId: string): Promise<void> {
     .then((snap) => {
       snap.forEach((item) => avgRatingsArr.push(item.data().rating))
     })
-  console.debug('arr', avgRatingsArr)
 
   // おすすめ度の平均値を算出
   let sum: number = 0 // 合計値
@@ -25,21 +22,18 @@ export async function setAvgRating(classId: string): Promise<void> {
   }
   const nonRoundedSum = sum / avgRatingsArr.length
   const avgRating = Math.round(nonRoundedSum * 10) / 10 // 小数点第2位を四捨五入
-  console.debug('avgRating', avgRating)
   // avgRating が NaN でなければそのままセット
   if (!isNaN(avgRating)) {
-    console.debug('avgRating', avgRating)
     await db.collection('classes').doc(classId).update({
       avgRating
     })
   }
   // avgRating が NaN(クチコミが存在しない)の場合 デフォルト値として ０ をセット
   else if (isNaN(avgRating)) {
-    console.debug('avgRating', avgRating)
     await db.collection('classes').doc(classId).update({
       avgRating: 0
     })
   } else {
-    console.debug('error')
+    console.error('error')
   }
 }
