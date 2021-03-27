4ed44a7109d46ae740f2bff5264ef0be5fe08c5f
tomo <tomohiroyoshida0511@gmail.com>
2021-03-27 18:31:31 +0900

    Add: tooltip

--- pages/search/_id.vue
+++ pages/search/_id.vue
@@ -58,18 +58,27 @@
               outlined
             >
               <!-- アイコン＋ユーザー名 -->
-              <div
-                class="d-flex mx-1 mt-2 user-info"
-                @click="goToUserPage(item.uid)"
-              >
-                <!-- TODO: プロフィール画像  -->
-                <!-- <v-img :src="getUserPhotoURL()" class="image" /> -->
-                <v-icon color="primary"> mdi-account-circle </v-icon>
-                <a class="text-body-2 pa-1">
-                  <span class="black--text">
-                    {{ getUsername(item.uid) }}
-                  </span>
-                </a>
+              <div class="user-info">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <div
+                      class="d-flex mx-1 mt-2"
+                      @click.stop="goToUserPage(item.uid)"
+                    >
+                      <!-- TODO: プロフィール画像  -->
+                      <!-- <v-img :src="getUserPhotoURL()" class="image" /> -->
+                      <v-icon color="primary" v-bind="attrs" v-on="on">
+                        mdi-account-circle
+                      </v-icon>
+                      <a class="text-body-2 pa-1" v-bind="attrs" v-on="on">
+                        <span class="black--text">
+                          {{ getUsername(item.uid) }}
+                        </span>
+                      </a>
+                    </div>
+                  </template>
+                  <span>プロフィール詳細へ</span>
+                </v-tooltip>
               </div>
 
               <!-- タイトル＋レーティング -->
@@ -98,24 +107,57 @@
               </div>
               <!-- 編集・削除ボタン -->
               <div v-if="uid === item.uid" class="buttons">
-                <v-btn fab icon small @click="openUpdateDialog(item)">
-                  <v-icon color="teal lighten-2">mdi-pencil-outline</v-icon>
-                </v-btn>
-                <v-btn fab icon small @click="openDeleteConfirm(item.docId)">
-                  <v-icon color="red lighten-1">mdi-trash-can-outline</v-icon>
-                </v-btn>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      fab
+                      icon
+                      small
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="openUpdateDialog(item)"
+                    >
+                      <v-icon color="teal lighten-2">mdi-pencil-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>クチコミ編集</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      fab
+                      icon
+                      small
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="openDeleteConfirm(item.docId)"
+                    >
+                      <v-icon color="grey darken-2"
+                        >mdi-trash-can-outline</v-icon
+                      >
+                    </v-btn>
+                  </template>
+                  <span>クチコミ削除</span>
+                </v-tooltip>
               </div>
               <!-- いいねボタン -->
-              <div icon small class="d-flex justify-center">
-                <v-icon
-                  :id="item.docId"
-                  class="likes-icon"
-                  @click="clickHeart(item.docId)"
-                  >mdi-heart-outline</v-icon
-                >
-                <div class="text-body-2 ml-2 mt-2">
-                  {{ getLikesCount(item.docId) || 0 }}
-                </div>
+              <div class="d-flex justify-center">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      :id="item.docId"
+                      class="likes-icon"
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="clickHeart(item.docId)"
+                      >mdi-heart-outline</v-icon
+                    >
+                    <div class="text-body-2 ml-2 mt-2">
+                      {{ getLikesCount(item.docId) || 0 }}
+                    </div>
+                  </template>
+                  <span>いいね</span>
+                </v-tooltip>
               </div>
             </v-card>
           </v-col>
@@ -439,7 +481,7 @@ export default defineComponent({
 }
 /** ユーザーの情報 */
 .user-info {
-  width: 280px;
+  width: 250px;
 }
 a {
   text-decoration: none;
