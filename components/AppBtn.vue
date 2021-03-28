21312e9f1c961bd2f0fcc407f9ee49e9a5441635
tomo <tomohiroyoshida0511@gmail.com>
2021-03-28 14:46:20 +0900

    Update: add props "text" & "width"

--- components/AppBtn.vue
+++ components/AppBtn.vue
@@ -1,8 +1,8 @@
 <template>
   <v-btn
     v-bind="$attrs"
-    :text="$attrs.text || 'text'"
-    :width="$attrs.width || '10rem'"
+    :text="text || 'text'"
+    :width="width || '10rem'"
     class="app-btn white--text"
     @click="$emit('click', $event)"
   >
@@ -13,6 +13,14 @@
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 export default defineComponent({
-  name: 'AppBtn'
+  name: 'AppBtn',
+  props: {
+    text: {
+      type: String
+    },
+    width: {
+      type: String
+    }
+  }
 })
 </script>
