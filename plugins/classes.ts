3c6126447e99cfbd8e4af0cd010f0323ed593d5e
tomo <tomohiroyoshida0511@gmail.com>
2021-04-05 23:19:21 +0900

    Chore: remove meaningless codes

--- plugins/classes.ts
+++ plugins/classes.ts
@@ -5,17 +5,14 @@ import { Class } from '@/types/State'
 
 export default defineNuxtPlugin(async ({ store }) => {
   const fetchedClasses: Class[] = []
-  const storeClasses: Class[] = store.getters.classes as Class[]
-  if (!storeClasses.length) {
-    await db
-      .collection('classes')
-      .orderBy('classTitle', 'asc')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          fetchedClasses.push(doc.data() as Class)
-        })
+  await db
+    .collection('classes')
+    .orderBy('classTitle', 'asc')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        fetchedClasses.push(doc.data() as Class)
       })
-    store.dispatch('setClasses', fetchedClasses)
-  }
+    })
+  store.dispatch('setClasses', fetchedClasses)
 })
